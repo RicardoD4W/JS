ea54e064299d2b0b67384746aba0b375d6929fb7
RicardoD4W <ricardo.rodriguez@fernando3martos.com>
2022-08-27 00:11:53 +0200

    add

---
+++ master/navigator/main.js
@@ -0,0 +1,12 @@
+console.log(navigator.appCodeName);  console.log(" ");
+console.log(navigator.appName);  console.log(" ");
+console.log(navigator.appVersion);  console.log(" ");
+console.log(navigator.connection);  console.log(" ");
+console.log(navigator.geolocation);  console.log(" ");
+console.log(navigator.hardwareConcurrency);  console.log(" ");
+console.log(navigator.language);  console.log(" ");
+console.log(navigator.languages);  console.log(" ");
+console.log(navigator.mimeTypes);  console.log(" ");
+console.log(navigator.onLine);  console.log(" ");
+console.log(navigator.userAgent);  console.log(" ");
+console.log(navigator.cookieEnabled);  console.log(" ");
